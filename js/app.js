ec1c94a4af98c739171047725b56c2fd1e267942
shirmung <shirmung@gmail.com>
2013-10-05 15:01:41 -0400

    basic JS pinging logic

--- js/app.js
+++ js/app.js
@@ -1,3 +1,54 @@
+function completed() {
+  // $.ajax({
+  //   url: "service.php",
+  //   data: // status (win or lose)
+  //   type: "POST",
+  //   success: function (data) {
+  //     console.log("post");
+  //   }
+  // });
+};
+
+// game play
+function callMe() {
+  console.log("hello");
+  
+  // game logic, click with timer
+  
+  // success message
+  // failure message
+  
+  completed();
+};
+
+var infiniteRequest = function() {
+  $.ajax({
+    url: "get_potato.php",
+    success: function(data) {
+      var random = Math.ceil(Math.random() * 10) * 1000;
+
+      if (data) {
+        callMe();
+      } else {
+        setTimeout(function() {
+          infiniteRequest();
+        }, random);
+      }
+    }
+  });
+};
+
 (function(){
-  console.log('here');
+  var uniqueID = "id" + (new Date()).getTime();
+
+  $.ajax({
+    url: "add_user.php",
+    data: { "id": uniqueID },
+    type: "POST",
+    success: function (data) {
+      console.log(data);
+    }
+  });
+
+  infiniteRequest();
 })();
