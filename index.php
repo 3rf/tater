cb6f09a49b102942a63896f0898d1b0ee0f96910
shirmung <shirmung@gmail.com>
2013-10-05 14:00:57 -0400

    changing a few more things

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Hot Potato</title>
         <link rel="stylesheet" href="css/style.css">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="js/app.js"></script>
     </head>
     <body>
         <p>Hello world! This is HTML5 Boilerplate.</p>
-
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-        <script src="js/app.js"></script>
     </body>
 </html>
