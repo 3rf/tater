4a326acdff23f6be343c369471770ecc91d9db3a
Pritika Nilaratna <pritika.16@gmail.com>
2013-10-05 16:12:15 -0400

    Defining status to complete game
    Signed-off-by: Pritika Nilaratna <pritika.16@gmail.com>

--- get_potato.php
+++ get_potato.php
@@ -1,23 +1,34 @@
 <?php
 	include_once('bootstrap.php');
 
-	// Set options
-	$options = array('dir' => 'storage');
-	     
-	// Load the databases
-	$users = Flintstone::load('users', $options);
-
-	// Retrieve all key names
-    $keys = $users->getKeys(); // returns array('bob', 'joe', ...)
-    if($keys)
-    {
-    	$key = array_rand($keys);
-    	echo $keys[$key];
-    	exit;
-    }
-    else
-    {
-    	header("HTTP/1.0 404 Not Found");
+	//check if potato in progress
+	if(isset($_SESSION['status']) && $_SESSION['status'] == 1)
+	{
+		header("HTTP/1.0 404 Not Found");
 		exit;
-    }
+	}
+	else
+	{
+		// Set options
+		$options = array('dir' => 'storage');
+		     
+		// Load the databases
+		$users = Flintstone::load('users', $options);
+
+		// Retrieve all key names
+	    $keys = $users->getKeys(); // returns array('bob', 'joe', ...)
+	    if($keys)
+	    {
+	    	$_SESSION['status'] = 1;
+	    	$key = array_rand($keys);
+	    	echo $keys[$key];
+	    	exit;
+	    }
+	    else
+	    {
+	    	header("HTTP/1.0 404 Not Found");
+			exit;
+	    }
+	}
+	
 ?>
---
+++ status.php
@@ -0,0 +1,8 @@
+<?php
+	// 0 is available
+	//1 is engaged
+	if(isset($_POST['status'])
+	{
+		$_SESSION['status'] = $_POST['status'];
+	}
+?>
