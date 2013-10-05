72e0983a03d4e0fb6c641fcce52e4d26e41857f9
shirmung <shirmung@gmail.com>
2013-10-05 15:54:55 -0400

    potato movements

--- js/app.js
+++ js/app.js
@@ -9,12 +9,30 @@ function completed() {
   // });
 };
 
+function newPosition() {
+  var h = $(window).height() - 606;
+  var w = $(window).width() - 517;
+
+  var nh = Math.floor(Math.random() * h);
+  var nw = Math.floor(Math.random() * w);
+
+  return [nh, nw];
+};
+
+function animateXYZ() {
+  var position = newPosition();
+
+  $('#potato').animate({ top: position[0], left: position[1] }, function() {
+    animateXYZ();   
+  });
+};
+
 // game play
 function play() {
-  console.log('playing');
-
   $('<div id="potato"></div>').appendTo('body');
   
+  animateXYZ();
+
   // game logic, click with timer
 
   // success message
@@ -30,7 +48,7 @@ var infiniteRequest = function(uniqueID) {
       if (data === uniqueID) {
         play();
       } else {
-        var random = Math.ceil(Math.random() * 10) * 1000;
+        var random = Math.ceil(Math.random() * 5) * 1000;
         
         setTimeout(function() {
           infiniteRequest(uniqueID);
